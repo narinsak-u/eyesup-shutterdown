5a7185a09f561fb8c4ec9e10f55fb90ad98a9b14
narinsak.u <nawongsak@gmail.com>
2026-06-14 13:16:17 +0700

    fix: add eslint-disable for any cast in GalleryItem spec

--- src/__tests__/GalleryItem.spec.ts
+++ src/__tests__/GalleryItem.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import GalleryItem from '../components/GalleryItem.vue'
